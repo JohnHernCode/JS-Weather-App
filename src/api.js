60c6d244fc08e6248b08f2a92c109c7789159de2
JohnHernCode <john@writecode.ninja>
2021-05-31 23:35:22 -0600

    Add https instead of http for mixed issue with firefox browser

--- src/api.js
+++ src/api.js
@@ -9,7 +9,7 @@ const weather = (() => {
     }
 
     async function getData(city) {
-        const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=7e80b833710868e5ad24105caf405553`;
+        const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=7e80b833710868e5ad24105caf405553`;
         try {
             const response = await fetch(
                 url,
